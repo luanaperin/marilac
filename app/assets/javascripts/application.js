38e002df5e52460c89ca2310a73851a480bdc127
Bruno Cerqueira <brunocerqueiram@gmail.com>
2017-08-31 17:09:27 -0300

    Navbar acabado

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,19 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function() {
+
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the
+      //nav bar to stick.
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 40) {
+      $('.nav-index').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 40) {
+      $('.nav-index').removeClass('navbar-fixed');
+    }
+  });
+});
